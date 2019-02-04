e9821e323e3b999e1653a49c4a3dbaa59aa5d223
George  Pianka <gpianka1@gmail.com>
2019-02-04 07:52:28 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -15,12 +15,10 @@ class Song < ActiveRecord::Base
     self.genre = Genre.find_or_create_by(name: name)
   end
 
-   def genre_name
+  def genre_name
     self.genre ? self.genre.name : nil
   end
 
- # .build creates objects without saving them -- we are saving in controller
-# basically the same as .new
   def note_contents=(notes)
     notes.each do |content|
       if content.strip != ''
